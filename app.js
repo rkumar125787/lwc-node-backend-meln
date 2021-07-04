132ae940a89180d1a9c86f8e670042f0f0f65f9e
Rajesh Kumar <rkumar125787@gmail.com>
2021-07-04 23:26:57 +0530

    connection setup

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ const express = require('express');
 const port = 5000;
 const app = express();
 
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+app.use(bodyParser.json());
+
+// app.use('/api/accounts', accountsRoute);
+
 app.get('/', (req, res, next) => {
     res.status(200).send({ status: `tested` });
 })
-app.listen(process.env.PORT || port, (req, res, next) => {
-    console.log('listening on port')
-})
+// app.listen(process.env.PORT || port, (req, res, next) => {
+//     console.log('listening on port')
+// })
+mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@cluster0.wpbzh.mongodb.net/sfdc?retryWrites=true&w=majority`).
+    then(() => {
+        app.listen(process.env.PORT || port);
+        console.log('listening')
+    }).
+    catch(
+        err => {
+            console.log(err);
+        }
+    )
