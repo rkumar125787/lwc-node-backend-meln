77deec3399c78dd31e4673bfcf17e764cdb7dff0
Rajesh Kumar <rkumar125787@gmail.com>
2021-07-04 23:56:52 +0530

    added router and controller

--- controllers/account-controller.js
+++ controllers/account-controller.js
@@ -1,5 +1,6 @@
 const createAccount = (req, res, next) => {
     const { name, address } = req.body;
+    console.log('req.body' + JSON.stringify(req.body));
     res.status('200').send({ name: name, address: address });
 }
 
