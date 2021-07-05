4bb34891c62d4a883eaacaa2cbe83a058a48224b
Rajesh Kumar <rkumar125787@gmail.com>
2021-07-05 12:12:41 +0530

    Added Headers

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use(bodyParser.json());
 
 app.use('/', accountRoutes);
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+    next();
+});
+app.use((req, res, next) => {
     const error = new HttpError('Could not find this route.', 404);
     throw error;
 });
