b25e7d714dbfbe76024edcd893cd099034be92f3
Rajesh Kumar <rkumar125787@gmail.com>
2021-07-05 18:01:03 +0530

    added validation

--- routes/contact-routes.js
+++ routes/contact-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contact-controller')
-router.post('api/contacts/create', contactController.createContact);
+router.post('/api/contacts/create', contactController.createContact);
 router.get('/', contactController.getContacts);
 module.exports = router;
 
