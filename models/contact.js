23383bf54ed4386948b1f83aeab331cabf29aa24
Rajesh Kumar <rkumar125787@gmail.com>
2021-07-05 17:53:54 +0530

    added validation

--- models/contact.js
+++ models/contact.js
@@ -2,8 +2,8 @@ const mongoose = require(`mongoose`);
 const Schema = mongoose.Schema;
 const contactSchema = new Schema({
     firstname: { type: String },
-    lastname: { type: String, required: [true, 'Last name is required'] },
-    email: { type: String, required: [true, 'email is required'] },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
     creator: { type: String, required: true },
     createdbysfid: { type: String, required: true }
 })
